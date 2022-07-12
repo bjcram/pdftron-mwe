b3adc6d1707db476486e1a8704558908536e9169
Schwier, Christian <Christian.Schwier@umlaut.com>
2022-07-12 09:07:59 +0200

    feat: add more pdf files

--- src/main/java/org/umlaut/pdftron/mwe/Application.java
+++ src/main/java/org/umlaut/pdftron/mwe/Application.java
@@ -12,19 +12,25 @@ import org.springframework.util.ResourceUtils;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.stream.Stream;
 
 @SpringBootApplication(scanBasePackages = "org.umlaut.pdftron.mwe", exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
-    private static Logger log = LoggerFactory.getLogger(Application.class);
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
 
     public static void main(String[] args) throws Exception {
         final ConfigurableApplicationContext context = SpringApplication.run(Application.class);
         final PdfToHtmlParserService parserService = context.getBean(PdfToHtmlParserService.class);
-        final File file = ResourceUtils.getFile("classpath:Test.pdf");
 
-        try (FileInputStream inputStream = new FileInputStream(file)) {
-            log.info(parserService.convertToHtml(inputStream.readAllBytes()));
+        for(String fileName : List.of("Test1.pdf", "Test2.pdf", "Test3.pdf", "Test4.pdf")){
+            File file = ResourceUtils.getFile("classpath:" + fileName);
+            try (FileInputStream inputStream = new FileInputStream(file)) {
+                log.info(parserService.convertToHtml(inputStream.readAllBytes()));
+            } catch (Exception e) {
+                log.error("Failed to parse file {}.", file, e);
+            }
         }
     }
 }
